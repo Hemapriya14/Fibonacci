dfb098d3231fce5c8117772cd71635caa965a034
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-20 22:33:32 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package weeek1.day4;
+
+public class Fibonacci {
+public static void main(String[] args) {
+		
+		int f=0;
+		  int sum;
+		  System.out.println(f);
+		  int s=1;
+		  System.out.println(s);
+		  for (int i =2; i<=8; i++) {
+			  sum=f+s;
+			  System.out.println(sum);
+			  f=s;
+			  s=sum;
+		  }
+}
+}
